ce030ee7018277d587499642061bcbbc580f0573
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2016-10-12 23:01:57 +0300

    Expandable list view

---
+++ app/src/main/java/com/example/karolis/workitout/ChildInfo.java
@@ -0,0 +1,29 @@
+package com.example.karolis.workitout;
+
+/**
+ * Created by Karolis on 2016-10-12.
+ */
+
+
+public class ChildInfo {
+
+    private String sequence = "";
+    private String name = "";
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- app/src/main/java/com/example/karolis/workitout/MainActivity.java
+++ app/src/main/java/com/example/karolis/workitout/MainActivity.java
@@ -1,32 +1,29 @@
 package com.example.karolis.workitout;
 
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
 
-import com.example.karolis.workitout.dataobjects.Point;
-
-import org.w3c.dom.Text;
-
-import java.text.DecimalFormat;
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 public class MainActivity extends AppCompatActivity{
 
-    //float[] gravity = new float[3];
-    //float[] linear_acceleration = new float[3];
+    private LinkedHashMap<String, GroupInfo> subjects = new LinkedHashMap<String, GroupInfo>();
+    private ArrayList<GroupInfo> deptList = new ArrayList<GroupInfo>();
+
+    private CustomAdapter listAdapter;
+    private ExpandableListView expandableWorkoutList;
+
+    private int selectedGroup = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,23 +31,133 @@ public class MainActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        // add data for displaying in expandable list view
+        loadData();
+
+        //get reference of the ExpandableListView
+        expandableWorkoutList = (ExpandableListView) findViewById(R.id.expandable_workout_list);
+        // create the adapter by passing your ArrayList data
+        listAdapter = new CustomAdapter(MainActivity.this, deptList);
+        // attach the adapter to the expandable list view
+        expandableWorkoutList.setAdapter(listAdapter);
+
+        //collapse all the Groups
+        collapseAll();
+
+        expandableWorkoutList.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if(groupPosition != selectedGroup || selectedGroup == -1) {
+                   return false;
+                }
+                return true;
             }
-        });*/
+        });
+        //Responsible for expanding only one group.
+        expandableWorkoutList.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if(groupPosition != selectedGroup) {
+                    expandableWorkoutList.collapseGroup(selectedGroup);
+                }
+                selectedGroup = groupPosition;
+            }
+        });
+
     }
 
+    //method to expand all groups
+    private void expandAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            expandableWorkoutList.expandGroup(i);
+        }
+    }
+
+    //method to collapse all groups
+    private void collapseAll() {
+        int count = listAdapter.getGroupCount();
+        for (int i = 0; i < count; i++){
+            expandableWorkoutList.collapseGroup(i);
+        }
+    }
+
+    //load some initial data into out list
+    private void loadData(){
+        //TODO Replace with sqlite fetch
+        addProduct("Android","ListView");
+        addProduct("Android","ExpandableListView");
+        addProduct("Android","GridView");
+
+        addProduct("Java","PolyMorphism");
+        addProduct("Java","Collections");
+        addProduct("Java1","Collections");
+        addProduct("Java2","Collections");
+        addProduct("Java3","Collections");
+        addProduct("Java4","Collections");
+        addProduct("Java5","Collections");addProduct("Java","Collections");
+        addProduct("Java6","Collections");
+        addProduct("Java7","Collections");
+        addProduct("Java8","Collections");
+        addProduct("Java89","Collections");
+        addProduct("Java9","Collections");
+        addProduct("Java10","Collections");
+        addProduct("Java11","Collections");
+        addProduct("Java12","Collections");
+
+
+
+
+    }
+
+
+
+    //here we maintain our products in various departments
+    private int addProduct(String department, String product){
+        //TODO replace with actual logic
+        int groupPosition = 0;
+
+        //check the hash map if the group already exists
+        GroupInfo headerInfo = subjects.get(department);
+        //add the group if doesn't exists
+        if(headerInfo == null){
+            headerInfo = new GroupInfo();
+            headerInfo.setName(department);
+            subjects.put(department, headerInfo);
+            deptList.add(headerInfo);
+        }
+
+        //get the children for the group
+        ArrayList<ChildInfo> productList = headerInfo.getProductList();
+        //size of the children list
+        int listSize = productList.size();
+        //add to the counter
+        listSize++;
+
+        //create a new child and add that to the group
+        ChildInfo detailInfo = new ChildInfo();
+        detailInfo.setSequence(String.valueOf(listSize));
+        detailInfo.setName(product);
+        productList.add(detailInfo);
+        headerInfo.setProductList(productList);
+
+        //find the group position inside the list
+        groupPosition = deptList.indexOf(headerInfo);
+        return groupPosition;
+    }
+
+
+
+
+
+
+
+    //Triggers when floating start button is clicked
     public void onClickStart(View view){
         Intent intent = new Intent(this, ExerciseActivity.class);
         startActivity(intent);
     }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
