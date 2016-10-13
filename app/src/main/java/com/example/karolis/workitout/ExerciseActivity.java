97ff1c6fd4f72f60dd7e56e53268ca6e8aee054b
Karolis_Uzdnavicius <uzda.kara@gmail.com>
2016-10-13 20:15:33 +0300

    Cleaned up accelerometer

--- app/src/main/java/com/example/karolis/workitout/ExerciseActivity.java
+++ app/src/main/java/com/example/karolis/workitout/ExerciseActivity.java
@@ -30,7 +30,7 @@ public class ExerciseActivity extends AppCompatActivity  implements SensorEventL
         text.setText("event not triggered");
 
         mSensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         point = new Point();
         lastUpdateTime = Calendar.getInstance().getTimeInMillis();
@@ -50,42 +50,10 @@ public class ExerciseActivity extends AppCompatActivity  implements SensorEventL
 
 
     public void onSensorChanged(SensorEvent event){
-        // In this example, alpha is calculated as t / (t + dT),
-        // where t is the low-pass filter's time-constant and
-        // dT is the event delivery rate.
-        Point acceleration = new Point();
-        float timeTmp = Calendar.getInstance().getTimeInMillis();
-        float deltaTime = timeTmp - lastUpdateTime;
-        lastUpdateTime = timeTmp;
-        /*final float alpha = 0.8f;
+        point.setX(event.values[0]);
+        point.setY(event.values[1]);
+        point.setZ(event.values[2]);
 
-        // Isolate the force of gravity with the low-pass filter.
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-        // Remove the gravity contribution with the high-pass filter.
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];*/
-
-        if(gravity == null){
-            gravity = new Point();
-            gravity.setX(event.values[0]);
-            gravity.setY(event.values[1]);
-            gravity.setZ(event.values[2]);
-        }
-
-        acceleration.setX(event.values[0]);
-        acceleration.setY(event.values[1]);
-        acceleration.setZ(event.values[2]);
-
-        acceleration.sub(gravity);
-        //acceleration.times(deltaTime);
-        acceleration.sub(velocity);
-        velocity = acceleration;
-
-        point = velocity;//.times(deltaTime);
         text.setText(point.toString());
         // text.setText("Gravity values: " +df.format(gravity[0]) + "\n" + df.format(gravity[1])+ "\n" + df.format(gravity[2])+
         //        "\nLinear acceleration: " + df.format(linear_acceleration[0]) + "\n" + df.format(linear_acceleration[1])+ "\n" + df.format(linear_acceleration[2]));
